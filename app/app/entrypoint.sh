9a86389b3ef27113873f4402487eaad39a14eb0e
Doxy <m.v.orekhov@gmail.com>
2026-02-13 00:12:31 +0100

    CD implementation

---
+++ app/app/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+echo "Applying database migrations..."
+python manage.py migrate --noinput
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+echo "Starting Gunicorn server..."
+exec gunicorn --bind 0.0.0.0:8000 wsgi:application
